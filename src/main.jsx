313f01247cc387d586444b276129781fa50fb183
trikmgithub <trikmforwork@gmail.com>
2025-01-09 16:38:25 +0700

    Reset css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalStyles from './components/GlobalStyles';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
     </StrictMode>,
 );
