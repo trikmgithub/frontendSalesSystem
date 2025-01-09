76b6877d9b324d27a8aaaf44598a7bb5e9db37e5
trikmgithub <trikmforwork@gmail.com>
2025-01-09 16:21:04 +0700

    Build layout

---
+++ src/routes/routes.jsx
@@ -0,0 +1,13 @@
+import config from '~/config';
+import { HeaderOnly } from '~/layouts/HeaderOnly';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.profile, component: Profile, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
